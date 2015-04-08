ca06f824fa86059013034d969de5e410c5107d0d
nicolas-f <github@nettrader.fr>
2015-04-08 11:45:37 +0200

    Add read buffer samples

---
+++ app/src/main/java/org/orbisgis/protonomap/CoreSignalProcessing.java
@@ -0,0 +1,19 @@
+package org.orbisgis.protonomap;
+
+/**
+ * Signal processing core
+ */
+public class CoreSignalProcessing {
+    private int sampleRate;
+
+    public CoreSignalProcessing(int sampleRate) {
+        this.sampleRate = sampleRate;
+    }
+
+    /**
+     * @param samples 1sec samples
+     */
+    public void setSamples(double[] samples) {
+
+    }
+}
--- app/src/main/java/org/orbisgis/protonomap/SignalProcessing.java
+++ app/src/main/java/org/orbisgis/protonomap/SignalProcessing.java
@@ -1,9 +1,11 @@
 package org.orbisgis.protonomap;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +16,10 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
 
 public class SignalProcessing extends ActionBarActivity {
 
@@ -22,8 +28,10 @@ public class SignalProcessing extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signal_processing);
         if (savedInstanceState == null) {
+            PlaceholderFragment frag = new PlaceholderFragment();
+            frag.setContext(getApplicationContext());
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, frag)
                     .commit();
         }
     }
@@ -55,8 +63,10 @@ public class SignalProcessing extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        private Context context;
 
-        public PlaceholderFragment() {
+        public void setContext(Context context) {
+            this.context = context;
         }
 
         @Override
@@ -64,21 +74,44 @@ public class SignalProcessing extends ActionBarActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_signal_processing, container, false);
             Button button = (Button) rootView.findViewById(R.id.doprocess);
-            button.setOnClickListener(new DoProcessing((TextView)rootView.findViewById(R.id.textView)));
+            button.setOnClickListener(new DoProcessing((TextView)rootView.findViewById(R.id.textView), context));
             return rootView;
         }
     }
 
     private static class DoProcessing implements CompoundButton.OnClickListener {
         private TextView textView;
+        private Context context;
 
-        private DoProcessing(TextView textView) {
+        private DoProcessing(TextView textView, Context context) {
             this.textView = textView;
+            this.context = context;
         }
 
         @Override
         public void onClick(View v) {
-
+            InputStream inputStream = context.getResources().openRawResource(R.raw.record_test);
+            try {
+                final int rate = 16000;
+                double[] secondSample = new double[rate];
+                int secondCursor = 0;
+                byte[] buffer = new byte[4096];
+                int read = 0;
+                CoreSignalProcessing signalProcessing = new CoreSignalProcessing(rate);
+                while ((read = inputStream.read(buffer)) != -1) {
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+                    double[] samples = byteBuffer.asDoubleBuffer().array();
+                    System.arraycopy(samples, 0, secondSample, secondCursor, Math.min(samples.length,rate - secondCursor));
+                }
+            } catch (IOException ex) {
+                Log.e(ex.getLocalizedMessage(),"Processing failed", ex);
+            } finally {
+                try {
+                    inputStream.close();
+                } catch (IOException ex) {
+                    Log.e(ex.getLocalizedMessage(),"Close file failed", ex);
+                }
+            }
         }
     }
 }

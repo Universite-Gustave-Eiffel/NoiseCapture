a5a782f46f88858c4f35ef0584c8a8123da7bfb8
nicolas-f <github@nettrader.fr>
2015-04-09 12:02:45 +0200

    Render value in textarea

--- app/src/main/java/org/orbisgis/protonomap/CoreSignalProcessing.java
+++ app/src/main/java/org/orbisgis/protonomap/CoreSignalProcessing.java
@@ -1,19 +1,82 @@
 package org.orbisgis.protonomap;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
+import java.util.Arrays;
+
 /**
  * Signal processing core
  */
 public class CoreSignalProcessing {
     private int sampleRate;
+    private PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+    private double[] spectrum;
+    public static String PROP_SPECTRUM = "PROP_SPECTRUM";
+
 
     public CoreSignalProcessing(int sampleRate) {
         this.sampleRate = sampleRate;
     }
 
     /**
+     * @return Last computed spectrum
+     */
+    public double[] getSpectrum() {
+        return spectrum;
+    }
+
+    public void processAudio(int encoding, final int rate, InputStream inputStream) throws IOException {
+        double[] secondSample = new double[rate];
+        int secondCursor = 0;
+        byte[] buffer = new byte[4096];
+        int read = 0;
+        // Read input signal up to buffer.length
+        while ((read = inputStream.read(buffer)) != -1) {
+            // Convert bytes into double values. Samples array size is 8 times inferior than buffer size
+            DoubleBuffer byteBuffer = ByteBuffer.wrap(buffer, 0, read).asDoubleBuffer();
+            double[] samples = new double[byteBuffer.capacity()];
+            byteBuffer.get(samples);
+            int lengthRead = Math.min(samples.length, rate - secondCursor);
+            // Copy sample fragment into second array
+            System.arraycopy(samples, 0, secondSample, secondCursor, lengthRead);
+            secondCursor+=lengthRead;
+            if(lengthRead < samples.length) {
+                setSamples(secondSample);
+                secondCursor = 0;
+                // Copy remaining sample fragment into new second array
+                int newLengthRead = samples.length - lengthRead;
+                System.arraycopy(samples, lengthRead, secondSample, secondCursor, newLengthRead);
+                secondCursor+=newLengthRead;
+            }
+            if(secondCursor == rate) {
+                setSamples(secondSample);
+                secondCursor = 0;
+            }
+        }
+    }
+
+    /**
      * @param samples 1sec samples
      */
     public void setSamples(double[] samples) {
+        setSpectrum(Arrays.copyOf(samples, 8));
+    }
 
+    public void addPropertyChangeListener(String property, PropertyChangeListener listener) {
+        listeners.addPropertyChangeListener(property, listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        listeners.removePropertyChangeListener(listener);
+    }
+
+    private void setSpectrum(double[] newSpectrum) {
+        double[] oldSpectrum = spectrum;
+        spectrum = newSpectrum;
+        listeners.firePropertyChange(PROP_SPECTRUM, oldSpectrum, spectrum);
     }
 }
--- app/src/main/java/org/orbisgis/protonomap/SignalProcessing.java
+++ app/src/main/java/org/orbisgis/protonomap/SignalProcessing.java
@@ -1,5 +1,6 @@
 package org.orbisgis.protonomap;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -16,9 +17,12 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
 
 
 public class SignalProcessing extends ActionBarActivity {
@@ -83,6 +87,7 @@ public class SignalProcessing extends ActionBarActivity {
         private TextView textView;
         private Context context;
 
+
         private DoProcessing(TextView textView, Context context) {
             this.textView = textView;
             this.context = context;
@@ -90,19 +95,28 @@ public class SignalProcessing extends ActionBarActivity {
 
         @Override
         public void onClick(View v) {
+            new Thread(new ProcessThread(textView, context)).start();
+        }
+    }
+
+    private static class ProcessThread implements Runnable {
+        private TextView textView;
+        private Context context;
+
+        private ProcessThread(TextView textView, Context context) {
+            this.textView = textView;
+            this.context = context;
+        }
+
+        @Override
+        public void run() {
+
             InputStream inputStream = context.getResources().openRawResource(R.raw.record_test);
             try {
                 final int rate = 16000;
-                double[] secondSample = new double[rate];
-                int secondCursor = 0;
-                byte[] buffer = new byte[4096];
-                int read = 0;
                 CoreSignalProcessing signalProcessing = new CoreSignalProcessing(rate);
-                while ((read = inputStream.read(buffer)) != -1) {
-                    ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
-                    double[] samples = byteBuffer.asDoubleBuffer().array();
-                    System.arraycopy(samples, 0, secondSample, secondCursor, Math.min(samples.length,rate - secondCursor));
-                }
+                signalProcessing.addPropertyChangeListener(CoreSignalProcessing.PROP_SPECTRUM ,new RenderSpectrum(textView, signalProcessing));
+                signalProcessing.processAudio(16, rate, inputStream);
             } catch (IOException ex) {
                 Log.e(ex.getLocalizedMessage(),"Processing failed", ex);
             } finally {
@@ -112,6 +126,29 @@ public class SignalProcessing extends ActionBarActivity {
                     Log.e(ex.getLocalizedMessage(),"Close file failed", ex);
                 }
             }
+        }
+    }
+
+    private static class RenderSpectrum implements PropertyChangeListener {
+        private TextView textView;
+        private CoreSignalProcessing signalProcessing;
+
+        private RenderSpectrum(TextView textView, CoreSignalProcessing signalProcessing) {
+            this.textView = textView;
+            this.signalProcessing = signalProcessing;
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent event) {
+            StringBuilder stringBuffer = new StringBuilder();
+            for(double level : signalProcessing.getSpectrum()) {
+                if(stringBuffer.length() != 0) {
+                    stringBuffer.append("\n");
+                }
+                stringBuffer.append(level);
+            }
+
+            textView.setText(stringBuffer.toString());
         }
     }
 }

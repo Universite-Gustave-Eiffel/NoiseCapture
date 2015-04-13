d2013677e391ad18630a1d1c40d7684a0b77dc5d
Picaut <judicael.picaut@ifsttar.fr>
2015-04-13 23:16:29 +0200

    Add drawer_list_item.xml in the Git directory

--- app/src/main/java/org/orbisgis/protonomap/Measurement.java
+++ app/src/main/java/org/orbisgis/protonomap/Measurement.java
@@ -10,9 +10,7 @@ import android.view.MenuItem;
 import android.graphics.Color;
 import android.content.Intent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.HorizontalBarChart;
@@ -25,12 +23,6 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.DataSet;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.filter.Approximator;
-import com.github.mikephil.charting.data.filter.Approximator.ApproximatorType;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.Highlight;
 import com.github.mikephil.charting.utils.ValueFormatter;
 
 import java.text.DecimalFormat;
@@ -40,14 +32,7 @@ public class Measurement extends MainActivity {
 
     public ImageButton button;
     static float Leqi;
-/*
-    // For the list view
-    private ListView mDrawerList;
-    private ArrayAdapter<String> mAdapter;
-    private DrawerLayout mDrawerLayout;
-    private String[] mMenuLeft;
-   // private ActionBarDrawerToggle mDrawerToggle;
-*/
+
     // For the Charts
     protected HorizontalBarChart mChart; // VUMETER representation
     protected BarChart sChart; // Spectrum representation
@@ -164,7 +149,6 @@ public class Measurement extends MainActivity {
 
     }
 
-
     // Fix the format of the dB Axis of the vumeter
     public class dBValueFormatter implements ValueFormatter {
 
@@ -247,95 +231,5 @@ public class Measurement extends MainActivity {
     public static final int[] SPECTRUM_COLORS = {
             Color.rgb(0, 128, 255), Color.rgb(102, 178, 255), Color.rgb(204, 229, 255),
     };
-
-    /*
-    public int[] getColors() {
-
-        int stacksize = 3;
-
-        // have as many colors as stack-values per entry
-        int []colors = new int[stacksize];
-
-        for(int i = 0; i < stacksize; i++) {
-            colors[i] = SPECTRUM_COLORS[i];
-        }
-
-        return colors;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_measurement, menu);
-        return true;
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    // Color for noise exposition representation
-    public static final int[] NE_COLORS = {
-            Color.rgb(255, 0, 0), Color.rgb(255, 128, 0), Color.rgb(255, 255, 0), Color.rgb(128, 255, 0), Color.rgb(0, 255, 0)
-    };
-    // Choose color category in function of sound level
-    public int getNEcatColors(float SL) {
-
-        int NbNEcat;
-
-        if(SL > 75.)
-        {
-            NbNEcat=0;
-        }
-        else if( (SL<=75) & (SL>65))
-        {
-            NbNEcat=1;
-        }
-        else if( (SL<=65) & (SL>55))
-        {
-            NbNEcat=2;
-        }
-        else if( (SL<=55) & (SL>45))
-        {
-            NbNEcat=3;
-        }
-        else
-        {
-            NbNEcat=4;
-        }
-        return NbNEcat;
-    }
-    */
 
 }
--- app/src/main/java/org/orbisgis/protonomap/Results.java
+++ app/src/main/java/org/orbisgis/protonomap/Results.java
@@ -1,21 +1,10 @@
 package org.orbisgis.protonomap;
 
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
-//import com.github.mikephil.charting.animation.EasingFunction;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.XAxis.XAxisPosition;
@@ -24,13 +13,9 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.components.Legend.LegendPosition;
-import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.utils.Highlight;
 import com.github.mikephil.charting.utils.PercentFormatter;
 
 import java.util.ArrayList;
@@ -38,15 +23,6 @@ import java.util.ArrayList;
 public class Results extends MainActivity {
 
     static float Leqi; // for testing
-
-    /*
-    // For the list view
-    private ListView mDrawerList;
-    private ArrayAdapter<String> mAdapter;
-    private DrawerLayout mDrawerLayout;
-    private String[] mMenuLeft;
-    */
-    //private ActionBarDrawerToggle mDrawerToggle;
 
     // For the Charts
     public PieChart rneChart;
@@ -255,79 +231,4 @@ public class Results extends MainActivity {
         //rneChart.invalidate();
     }
 
-    /*
-    // Color for noise exposition representation
-    public static final int[] NE_COLORS = {
-            Color.rgb(255, 0, 0), Color.rgb(255, 128, 0), Color.rgb(255, 255, 0), Color.rgb(128, 255, 0), Color.rgb(0, 255, 0)
-    };
-    // Choose color category in function of sound level
-    public int getNEcatColors(float SL) {
-
-        int NbNEcat;
-
-        if(SL > 75.)
-        {
-            NbNEcat=0;
-        }
-        else if( (SL<=75) & (SL>65))
-        {
-            NbNEcat=1;
-        }
-        else if( (SL<=65) & (SL>55))
-        {
-            NbNEcat=2;
-        }
-        else if( (SL<=55) & (SL>45))
-        {
-            NbNEcat=3;
-        }
-        else
-        {
-            NbNEcat=4;
-        }
-        return NbNEcat;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_measurement, menu);
-        return true;
-    }
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }

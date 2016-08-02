24b76d6c4842947cdb41f7922c3c9a00af91dac6
nicolas-f <github@nettrader.fr>
2016-08-02 10:54:47 +0200

    fix npe on comment activity

--- app/src/main/java/org/noise_planet/noisecapture/CommentActivity.java
+++ app/src/main/java/org/noise_planet/noisecapture/CommentActivity.java
@@ -28,6 +28,7 @@
 package org.noise_planet.noisecapture;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -37,6 +38,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -83,7 +85,9 @@ public class CommentActivity extends MainActivity {
         setContentView(R.layout.activity_comment);
 
         View mainView = findViewById(R.id.mainLayout);
-        mainView.setOnTouchListener(new MainOnTouchListener(this));
+        if(mainView != null) {
+            mainView.setOnTouchListener(new MainOnTouchListener(this));
+        }
 
         // Read record activity parameter
         // Use last record of no parameter provided

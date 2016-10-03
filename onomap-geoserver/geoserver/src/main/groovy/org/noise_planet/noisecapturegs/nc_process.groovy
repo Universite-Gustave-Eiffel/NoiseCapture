19a06370f6b8ba3d1ff1b7164d84152781368f31
nicolas-f <github@nettrader.fr>
2016-10-03 16:58:23 +0200

    fix groovy process script

--- onomap-geoserver/geoserver/src/main/groovy/org/noise_planet/noisecapturegs/nc_process.groovy
+++ onomap-geoserver/geoserver/src/main/groovy/org/noise_planet/noisecapturegs/nc_process.groovy
@@ -101,7 +101,7 @@ class Record{
  */
 def processArea(Hex hex, float range,float precisionFiler, Sql sql) {
     // A ratio < 1 add blank area between hexagons
-    float hexSizeRatio = 1.
+    float hexSizeRatio = 1.0;
     boolean doNotUseDeltaSigma = true
     def Pos center = hex.toMeter()
     def Coordinate centerCoord = center.toCoordinate();

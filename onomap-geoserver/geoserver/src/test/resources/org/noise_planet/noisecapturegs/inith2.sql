3f5b0a8017b74a4886a0c8f53adbddf7b855ad38
nicolas-f <fortin@ad.ifsttar.fr>
2017-09-18 10:19:05 +0200

    Fix sql

--- onomap-geoserver/geoserver/src/test/resources/org/noise_planet/noisecapturegs/inith2.sql
+++ onomap-geoserver/geoserver/src/test/resources/org/noise_planet/noisecapturegs/inith2.sql
@@ -21,8 +21,8 @@ COMMENT ON COLUMN NOISECAPTURE_USER.PROFILE IS 'User acoustic knowledge, one of 
 
 -- Table: NOISECAPTURE_PARTY
 CREATE TABLE NOISECAPTURE_PARTY (
-    PK_PARTY serial NOT NULL,
-    THE_GEOM geometry NOT NULL,
+    pk_party serial NOT NULL,
+    the_geom geometry NOT NULL,
     title varchar NOT NULL,
     tag varchar UNIQUE NOT NULL,
     description varchar NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE NOISECAPTURE_PARTY (
 COMMENT ON COLUMN NOISECAPTURE_PARTY.title IS 'Short NoiseParty title';
 COMMENT ON COLUMN NOISECAPTURE_PARTY.description IS 'Long description of the NoiseParty';
 COMMENT ON COLUMN NOISECAPTURE_PARTY.tag IS 'Tag typed by users';
-COMMENT ON COLUMN NOISECAPTURE_PARTY.geometry IS 'NoiseParty location';
+COMMENT ON COLUMN NOISECAPTURE_PARTY.the_geom IS 'NoiseParty location';
 
 -- Table: NOISECAPTURE_TRACK
 CREATE TABLE NOISECAPTURE_TRACK (

da5f2e5579ab71616b90fd983f815061b76bc01c
nicolas-f <[omitted]>
2017-11-09 09:29:53 +0100

    Generation of signal

--- sosfilter/src/main/java/org/noise_planet/acousticmodem/Settings.java
+++ sosfilter/src/main/java/org/noise_planet/acousticmodem/Settings.java
@@ -5,94 +5,64 @@ package org.noise_planet.acousticmodem;
  */
 public class Settings {
 
-    public final double kLowFrequency; //the lowest frequency used
-    public final double kFrequencyStep; //the distance between frequencies
+    final int samplingRate;
 
-    public final int kBytesPerDuration; //how wide is the data stream
-    // (rps - that is, the pre-encoding data stream, not the audio)
-    // (rps - kFrequencies must be this long)
+    final double wordTimeLength;
 
-    public final int kBitsPerByte = 8; //unlikely to change, I know
+    final int wordLength;
 
-    // Amplitude of each frequency in a frame.
-    public final double kAmplitude; /* (1/8) */
+    // hexadecimal DTMF tones (each tone is simply the sum of two sine waves.)
+    static final int[][] DTMF_WORDS = new int[][]{
+            {697, 1209}, // 0->1
+            {697, 1336}, // 1->2
+            {697, 1477}, // 2->3
+            {697, 1633}, // 3->A
+            {770, 1209}, // 4->4
+            {770, 1336}, // 5->5
+            {770, 1477}, // 6->6
+            {770, 1633}, // 7->B
+            {852, 1209}, // 8->7
+            {852, 1336}, // 9->8
+            {852, 1477}, // A->9
+            {852, 1633}, // B->C
+            {941, 1209}, // C->*
+            {941, 1336}, // D->0
+            {941, 1477}, // E->#
+            {941, 1633}, // F->D
+         };
 
-    // Sampling frequency (number of sample values per second)
-    public final double kSamplingFrequency; //rps - reduced to 11025 from 22050
-    // to enable the decoder to keep up with the audio on Motorola CLIQ
 
-    // Sound duration of encoded byte (in seconds)
-    public final double kDuration; // rps - increased from 0.1 to improve reliability on Android
+    final int[][] words;
 
-    // Number of samples per duration
-    public final int kSamplesPerDuration;
-
-    //This is used to convert the floats of the encoding to the bytes of the audio
-    public final int kFloatToByteShift;
-
-    // The length, in durations, of the hail sequence
-    public final int kDurationsPerHail;
-
-    // The frequency used in the initial hail of the key
-    public final int kHailFrequency;
-
-    //The frequencies we use for each of the 8 bits
-    public final int kBaseFrequency = 1000;
-    public final int[] kFrequencies = {kBaseFrequency,   //1000
-            (int)(kBaseFrequency * (float)27/24), //1125
-            (int)(kBaseFrequency * (float)30/24), //1250
-            (int)(kBaseFrequency * (float)36/24), //1500
-            (int)(kBaseFrequency * (float)40/24), //1666
-            (int)(kBaseFrequency * (float)48/24), //2000
-            (int)(kBaseFrequency * (float)54/24), //2250
-            (int)(kBaseFrequency * (float)60/24)};//2500
-
-    //The length, in durations, of the hail sequence
-    public final int kDurationsPerSOS;
-
-    // The frequency used to signal for help
-    public final int kSOSFrequency;
-
-    // The length, in durations, of some session timing jitters
-    public final int kPlayJitter;
-    public final int kSetupJitter;
-
-    // The length, in durations, of the CRC
-    public final int kDurationsPerCRC;
-
-    public Settings(double kLowFrequency, double kFrequencyStep, int kBytesPerDuration, double kAmplitude, double kSamplingFrequency, double kDuration, int kFloatToByteShift, int kDurationsPerHail, int kHailFrequency, int kDurationsPerSOS, int kSOSFrequency, int kPlayJitter, int kSetupJitter, int kDurationsPerCRC) {
-        this.kLowFrequency = kLowFrequency;
-        this.kFrequencyStep = kFrequencyStep;
-        this.kBytesPerDuration = kBytesPerDuration;
-        this.kAmplitude = kAmplitude;
-        this.kSamplingFrequency = kSamplingFrequency;
-        this.kDuration = kDuration;
-        this.kFloatToByteShift = kFloatToByteShift;
-        this.kDurationsPerHail = kDurationsPerHail;
-        this.kHailFrequency = kHailFrequency;
-        this.kDurationsPerSOS = kDurationsPerSOS;
-        this.kSOSFrequency = kSOSFrequency;
-        this.kPlayJitter = kPlayJitter;
-        this.kSetupJitter = kSetupJitter;
-        this.kDurationsPerCRC = kDurationsPerCRC;
-        kSamplesPerDuration = (int)(kSamplingFrequency * kDuration);
+    /**
+     *
+     * @param samplingRate Sample rate of signal
+     * @param wordTimeLength Time in seconds of a word
+     * @param words As the same output of wordsFrom8frequencies or DTMF_WORDS
+     */
+    public Settings(int samplingRate, double wordTimeLength, int[][] words) {
+        this.samplingRate = samplingRate;
+        this.wordTimeLength = wordTimeLength;
+        this.wordLength = (int)(samplingRate * wordTimeLength);
+        if(words.length != 16) {
+            throw new IllegalArgumentException("Should be limited to 16 words type");
+        }
+        this.words = words;
     }
 
-    public Settings() {
-        kLowFrequency = 600;
-        kFrequencyStep = 50;
-        kBytesPerDuration = 1;
-        kAmplitude = 0.125d;
-        kSamplingFrequency = 22050;
-        kDuration = 0.2;
-        kFloatToByteShift = 128;
-        kDurationsPerHail = 3;
-        kHailFrequency = 3000;
-        kDurationsPerSOS = 1;
-        kSOSFrequency = (int)(kBaseFrequency * (float)30/24);
-        kPlayJitter = 5;
-        kSetupJitter = 10;
-        kDurationsPerCRC = 1;
-        kSamplesPerDuration = (int)(kSamplingFrequency * kDuration);
+    /**
+     * Create word dictionary from 8 frequencies values (combine two frequencies to make a word)
+     * @param baseFrequencies 8 frequencies
+     * @return words dict for Settings constructor
+     */
+    public static int[][] wordsFrom8frequencies(int[] baseFrequencies) {
+        int[][] outWordDict = new int[16][2];
+        for(int i = 0; i < 4; i++) { // row
+            for(int j = 0; j < 4; j++) { // column
+                outWordDict[i*j + i][0] = baseFrequencies[i];
+                outWordDict[i*j + i][1] = baseFrequencies[j + 4];
+            }
+        }
+        return outWordDict;
     }
 }
--- sosfilter/src/test/java/org/noise_planet/acousticmodem/AcousticModemTest.java
+++ sosfilter/src/test/java/org/noise_planet/acousticmodem/AcousticModemTest.java
@@ -1,25 +1,27 @@
 package org.noise_planet.acousticmodem;
 
 import org.junit.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-
-import static org.junit.Assert.*;
+import org.orbisgis.sos.ThirdOctaveBandsFiltering;
 
 /**
  *
  */
 public class AcousticModemTest {
+    private static final int[] UT_FREQUENCIES = new int[]{
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[11],
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[12],
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[13],
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[14],
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[15],
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[16],
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[17],
+            (int)ThirdOctaveBandsFiltering.STANDARD_FREQUENCIES_REDUCED[18]
+    };
 
     @Test
     public void TestEncodeDecode() throws Exception {
         String messageInput = "U1_76.8";
-        AcousticModem acousticModem = new AcousticModem(new Settings());
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        acousticModem.encodeStream(new ByteArrayInputStream(messageInput.getBytes()), baos);
+        AcousticModem acousticModem = new AcousticModem(new Settings(44100, 0.250, Settings.wordsFrom8frequencies(UT_FREQUENCIES)));
 
-        byte[] samples = baos.toByteArray();
-        acousticModem.decode(, samples);
     }
 }

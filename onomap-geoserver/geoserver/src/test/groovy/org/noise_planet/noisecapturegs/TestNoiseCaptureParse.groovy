e314bbf190cd281824775ce268c94d8f96bdb444
nicolas-f <>
2019-03-21 16:08:02 +0100

    Export GeoJSON, when no altitude, do not write null value, but only lat long

--- onomap-geoserver/geoserver/src/test/groovy/org/noise_planet/noisecapturegs/TestNoiseCaptureParse.groovy
+++ onomap-geoserver/geoserver/src/test/groovy/org/noise_planet/noisecapturegs/TestNoiseCaptureParse.groovy
@@ -204,6 +204,20 @@ class TestNoiseCaptureParse  extends GroovyTestCase {
         assertNull(sql.firstRow("SELECT pk_party FROM  noisecapture_track").pk_party)
     }
 
+    // Test parse with coordinates without Z values (wifi)
+    void testParse2d() {
+        Sql sql = new Sql(connection)
+        sql.execute("INSERT INTO NOISECAPTURE_PARTY(the_geom, title, tag, description, layer_name) VALUES ('POLYGON((2.38717 48.8944,2.38717 48.8964,2.38917 48.8964,2.38917 48.8944,2.38717 48.8944))','ANQES 2017 event','ANQES2017'," +
+                "'8es Assises nationales de la qualité de l''environnement sonore','noisecapture_area_anqes2017');")
+        new nc_parse().processFile(connection,
+                new File(TestNoiseCaptureParse.getResource("track_aaa26b2a-3345-4e58-9055-1a6567b055ad.zip").file))
+        // Read db; check content
+        assertEquals(1, sql.firstRow("SELECT COUNT(*) cpt FROM  noisecapture_track").get("cpt"))
+        assertNull(sql.firstRow("SELECT pk_party FROM  noisecapture_track").pk_party)
+    }
+
+
+
 
     // Test parse with a party tag that exist in database
     void testParseNoiseParty() {

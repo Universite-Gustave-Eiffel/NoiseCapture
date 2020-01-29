6acbe611ba0edcd4905f959ba58a2fedf62ecac7
nicolas-f <[omitted]>
2020-01-29 11:29:33 +0100

    fix settings edit

--- app/src/main/java/org/noise_planet/noisecapture/SettingsActivity.java
+++ app/src/main/java/org/noise_planet/noisecapture/SettingsActivity.java
@@ -28,12 +28,14 @@
 package org.noise_planet.noisecapture;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SettingsActivity extends MainActivity {
+public class SettingsActivity extends MainActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +45,14 @@ public class SettingsActivity extends MainActivity {
 
     }
 
-
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if("settings_recording_gain".equals(key)) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("settings_calibration_method", String.valueOf(Storage.Record.CALIBRATION_METHODS.ManualSetting.ordinal()));
+            editor.apply();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.

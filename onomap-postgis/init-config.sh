3ceb793ae26809e6806c17f8c18c8f43fca70eb5
nicolas-f <github@nettrader.fr>
2025-06-03 17:11:43 +0200

    create postgres user to avoid issues with pgbackrest

--- onomap-postgis/init-config.sh
+++ onomap-postgis/init-config.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER onomap WITH PASSWORD '$POSTGRES_PASSWORD';
+	CREATE DATABASE noisecapture;
+	GRANT ALL PRIVILEGES ON DATABASE noisecapture TO onomap;
+EOSQL
+
 pgconf="$PGDATA/postgresql.conf"
 hbaconf="$PGDATA/pg_hba.conf"
 
 if ! grep -q "pgbackrest" "$pgconf"; then
   echo "wal_level = replica" >> $pgconf
   echo "archive_mode = on" >> $pgconf
-  echo "archive_command = 'pgbackrest --stanza=app archive-push %p'" >> $pgconf
+  echo "archive_command = 'pgbackrest --stanza=noisecapture archive-push %p'" >> $pgconf
   echo "archive_timeout = 1d" >> $pgconf
   echo "max_wal_senders = 3" >> $pgconf
 fi
-export PGUSER=onomap
+
 pgbackrest --stanza=noisecapture --log-level-console=info stanza-create

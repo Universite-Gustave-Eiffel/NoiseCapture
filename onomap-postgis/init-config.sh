6e610f6224cd37f3478f02b2f8728273c891c00b
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2025-06-17 11:22:29 +0200

    add metabase

--- onomap-postgis/init-config.sh
+++ onomap-postgis/init-config.sh
@@ -3,10 +3,10 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER onomap WITH PASSWORD '$POSTGRES_PASSWORD';
-	GRANT ALL PRIVILEGES ON DATABASE noisecapture TO onomap;
-	\c noisecapture
-	GRANT ALL ON SCHEMA public TO onomap;
-	ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO onomap;
+	ALTER DATABASE noisecapture OWNER TO onomap;
+	CREATE USER metabase WITH PASSWORD '$METABASE_PASSWORD';
+	CREATE DATABASE metabase;
+	ALTER DATABASE metabase OWNER TO metabase;
 EOSQL
 
 pgconf="$PGDATA/postgresql.conf"
@@ -16,7 +16,6 @@ if ! grep -q "pgbackrest" "$pgconf"; then
   echo "wal_level = replica" >> $pgconf
   echo "archive_mode = on" >> $pgconf
   echo "archive_command = 'pgbackrest --stanza=noisecapture archive-push %p'" >> $pgconf
-  echo "start-fast=y" >> $pgconf
   echo "max_wal_senders = 3" >> $pgconf
   echo "max_wal_size = 16GB" >> $pgconf
 fi

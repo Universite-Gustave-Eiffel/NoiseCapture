86ece9fec07f89354ce768562169a0168409f49d
nicolas <github@nettrader.fr>
2025-06-19 16:26:33 +0200

    fix configuration

--- onomap-postgis/init-config.sh
+++ onomap-postgis/init-config.sh
@@ -20,11 +20,11 @@ pgconf="$PGDATA/postgresql.conf"
 hbaconf="$PGDATA/pg_hba.conf"
 
 if ! grep -q "pgbackrest" "$pgconf"; then
-  echo "wal_level = replica" >> $pgconf
-  echo "archive_mode = on" >> $pgconf
-  echo "archive_command = 'pgbackrest --stanza=noisecapture archive-push %p'" >> $pgconf
-  echo "max_wal_senders = 3" >> $pgconf
-  echo "max_wal_size = 16GB" >> $pgconf
+  echo "wal_level = replica" >> "${pgconf}"
+  echo "archive_mode = on" >> "${pgconf}"
+  echo "archive_command = 'pgbackrest --stanza=noisecapture archive-push %p'" >> "${pgconf}"
+  echo "max_wal_senders = 3" >> "${pgconf}"
+  echo "max_wal_size = 16GB" >> "${pgconf}"
 fi
 
 pgbackrest --stanza=noisecapture --log-level-console=info stanza-create

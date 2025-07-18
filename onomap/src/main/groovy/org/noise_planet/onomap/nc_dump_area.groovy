cc404a4f9f2351e0df4cc165c15420f4e5d653ed
nicolas-f <github@nettrader.fr>
2025-07-18 11:48:19 +0200

    email send code

--- onomap/src/main/groovy/org/noise_planet/onomap/nc_dump_area.groovy
+++ onomap/src/main/groovy/org/noise_planet/onomap/nc_dump_area.groovy
@@ -33,9 +33,24 @@ import groovy.json.JsonSlurper
 import groovy.sql.GroovyResultSet
 import groovy.sql.Sql
 import groovy.transform.CompileStatic
+import groovy.transform.Field
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import javax.mail.Message
+import javax.mail.Authenticator
+import javax.mail.MessagingException
+import javax.mail.PasswordAuthentication
+import javax.mail.Multipart
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.AddressException
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.InternetHeaders
+import javax.mail.internet.MimeBodyPart
+import javax.mail.internet.MimeMessage
+import javax.mail.internet.MimeMultipart
+import javax.mail.internet.MimeUtility
 import javax.sql.DataSource
 import java.nio.file.Paths
 import java.sql.*
@@ -43,7 +58,6 @@ import java.time.DateTimeException
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import java.time.zone.ZoneRulesException
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeoutException
@@ -79,6 +93,8 @@ outputs = [
   result: [name: 'result', title: 'Created file', type: String.class]
 ]
 
+@Field
+static Logger LOGGER  = LoggerFactory.getLogger("logger_nc_dump_area")
 
 /**
  * Convert EPOCH time to ISO 8601
@@ -90,6 +106,108 @@ static def epochToRFCTime(long epochMillisecond) {
   return Instant.ofEpochMilli(epochMillisecond).atZone(ZoneId.of("UTC")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"))
 }
 
+/**
+ *  Create mail session.
+ *  @return mail session, may not be null.
+ */
+@CompileStatic
+static def createSession(Map input) {
+  String smtpProtocol = input.getOrDefault("smtpProtocol", null)
+  def smtpHost = input.getOrDefault("smtpHost", null)
+  def smtpPort = input.getOrDefault("smtpPort", 0)
+  String smtpPassword = input.getOrDefault("smtpPassword", null)
+  String smtpUsername = input.getOrDefault("smtpUsername", null)
+  boolean smtpDebug = input.getOrDefault("smtpDebug", false)
+
+  Properties props
+  try {
+    props = new Properties (System.getProperties());
+  } catch(SecurityException ex) {
+    props = new Properties()
+  }
+
+  String prefix = "mail.smtp"
+  if (smtpProtocol != null) {
+    props.put("mail.transport.protocol", smtpProtocol)
+    prefix = "mail." + smtpProtocol
+  }
+  if (smtpHost != null) {
+    props.put(prefix + ".host", smtpHost)
+  }
+  if (smtpPort > 0) {
+    props.put(prefix + ".port", String.valueOf(smtpPort))
+  }
+
+  Authenticator auth = null;
+  if(smtpPassword != null && smtpUsername != null) {
+    props.put(prefix + ".auth", "true")
+    auth = new Authenticator() {
+      protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(smtpUsername, smtpPassword)
+      }
+    };
+  }
+  Session session = Session.getInstance(props, auth);
+  if (smtpProtocol != null) {
+    session.setProtocolForAddress("rfc822", smtpProtocol)
+  }
+  if (smtpDebug) {
+    session.setDebug(smtpDebug)
+  }
+  return session
+}
+
+
+@CompileStatic
+static InternetAddress[] parseAddress(String addressStr) {
+  try {
+    return InternetAddress.parse(addressStr, true)
+  } catch(AddressException e) {
+    LOGGER.error("Could not parse address ["+addressStr+"].", e)
+    return null
+  }
+}
+
+@CompileStatic
+static def sendEmail(Map input, String recipient, String from, String subject, String body) {
+  try {
+    MimeBodyPart part
+    try {
+      ByteArrayOutputStream os = new ByteArrayOutputStream()
+      Writer writer = new OutputStreamWriter(
+        MimeUtility.encode(os, "quoted-printable"), "UTF-8")
+      writer.write(body)
+      writer.close()
+      InternetHeaders headers = new InternetHeaders()
+      headers.setHeader("Content-Type", "text/html; charset=UTF-8")
+      headers.setHeader("Content-Transfer-Encoding", "quoted-printable")
+      part = new MimeBodyPart(headers, os.toByteArray())
+    } catch(Exception ex) {
+      StringBuffer sbuf = new StringBuffer(body)
+      for (int i = 0; i < sbuf.length(); i++) {
+        if (sbuf.charAt(i) >= 0x80) {
+          sbuf.setCharAt(i, '?' as char)
+        }
+      }
+      part = new MimeBodyPart()
+      part.setContent(sbuf.toString(), "text/html")
+    }
+
+    Session session = createSession(input)
+    Message msg = new MimeMessage(session)
+    Multipart mp = new MimeMultipart()
+    mp.addBodyPart(part)
+    msg.setContent(mp)
+    msg.setSentDate(new Date(System.currentTimeMillis()))
+    msg.setFrom(from)
+    msg.setRecipients(Message.RecipientType.TO, parseAddress(recipient))
+    Transport.send(msg)
+  } catch(MessagingException e) {
+    LOGGER.error("Error occured while sending e-mail notification.", e);
+  } catch(RuntimeException e) {
+    LOGGER.error("Error occured while sending e-mail notification.", e);
+  }
+}
 
 /**
  * Convert EPOCH time to ISO 8601
@@ -101,8 +219,6 @@ static def epochToRFCTime(long epochMillisec, String zone) {
   try {
     zoneId = ZoneId.of(zone)
   } catch (DateTimeException ex) {
-    // skip
-  } catch (ZoneRulesException ex) {
     // skip
   }
   return Instant.ofEpochMilli(epochMillisec).atZone(zoneId).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
@@ -123,7 +239,6 @@ public String getDump(DataSource dataSource, File zipFileName, Map input) {
   // Maximum time to generate the dump
   final int MAX_GENERATE_DUMP_TIME = 15 * 60 * 1000
 
-  Logger logger = LoggerFactory.getLogger("logger_nc_dump_area")
   // gzip then base64 the content (http://www.txtwizard.net/compression)
   final String README_CONTENT = "H4sIAAAAAAAA/9VYbW/bNhD+nl9BBBjQbrYct02bFa4Bt2mzDunL5hb9aNDS2WJDkQpJ2XF//R5SlC2/NMuGrtuCwLAk8u655+4enjwohyMpmeFLlnHHWaqlpNRRxmZGF8zlxN5qYekFL11liI1UZrTI2KgsO0wklIQl56MPo+ej8ctue+3vfNlhHHsKnhHjCy4kn0pilcrIhG0DznJDs2fHuXPl015Pl6Q8ilQXhVY20WbekyIlZcn2dDaVvX5y0jsevsM6do6FU27piLX+LuvVgx4fJsC6YkbMc2eZUPjPxEJkFZcIUjlSuK1nAUcWTQW00WG2wbnlofn7u9gb2OxFA6KNec/VoFcOj44G5fBDxMlKoxEG8OXcMqWZpDlCmmmTAr/KmCWzIOtvIGR8FtwJrVhZmVIDC9NKrsDN1GpZOZIrb4OnaWV4umLYhABKSYBG1rJ5xQ0HTmICJOKBgONgGQsAImEeF69cro0N9CntmCFbggPhs+1xBI6FQV3Ve+N3pMHSdUUqpXUqqgCxdmBKQ65GH59uBRR5SAJFA5exVHJrnx1n3anU6ZUPpADB3anOVqhBc5Xppdq5qs2xz7bbXn08BOHnZFMjysb9TEiyT2tfdc/MSX+2eBiehNh9XkGPYoNUZzR8+X58wR49fPB40AvXQKRNJhR3BIpmZHzkzK6soyJGkT8Y/pg4pOLKJtH+oIeb3udLnuZsRjz0IdLBWUHc+gtwZiPM2rMTBU0kqbnLG9+WQHdm64TBdEHOrLwZz+tUo9iFmuPLjbcSDRehPkFmYNxGjJXEhxTDlqvX43dnj0/6a1+OG7djB5n1DuJK5reBBGTY4mGwP0C/7FmmUqf5rXbR2pUSN8EiFhQli0m9p6piig7264WUIjLAgAO0/8oVanvF+h3W//nJSYednDwN/+zizYf7NU3e5BetAtvrtpuuAmWeFSm+bJVnhNUJTeBvwOFMzNFa7VUW1efKHHaT3ZDLq0lIfhPwWizCXWAQhcd8RSsPEKgWXFZeYFcszbmaE1vmkEbv5YsoW5VZldAOD75y9bUSaDzPXUDEi82GLcv4IoW6ws6lcDn7MSk1OnNdm0FwUl1JWA69mzGn4UTgBt9U2XaeGx5KDovbzNk9RkLgk6oSWcPJh0DFRyUWZCyXELCPdTCvz+FBzASZGEIMso5EEdY3LE3JLQlE+ZhravY9z7lQE5/jqYnlWfsfi7mC4PrHnr/seWdTG+HMBA0mVnvIMmsZichs2wYyJA1xyBIva4EFhwgMVYSahb4vSFpwKEMG8cx5E4GYPdBhyyRsafBecrqujTRkxxLbIZrPbbPlo4/AVg0aPEnYGLkMhYZEZ+QPiHAQ8MzH7aGZ/WqWyCpOD3+SHDbdXsFOun10373oQ1VS3o8We0FygjZu19+dtNF3/WHZu6WHD2rdf789t4+O79CeB9R/7A+TWHFtoQ6S/w1OgVvt/yOnwUFM89LuRn7xfrxdUz7z/iwA+3rq0O3IAPlSbapyrGuuGwFpztpQww7TZN5q+WO4RA3cHIfoDufCo9pia3vT9+HngAq93WgZuxxBkYJ03hvdZ327u92WRFmb0zU94Ul9vNYjq9unQRuxqaUDNlrP/8RSMxV/Nb3rsRnBoJVwHgWdaSTo8dkPoe/Bn8u5i9pb0VaWfeOGPscCwzNR2Z2mg0p6zWv52FdFaAbfF8X32rou4k79eIiu2xnsOnUptlVTefXhm46sxx3czumGz2tt6J8WEehBjUxJysl1Q9nuW9JyuUwQEgb06RyqZhOM3b25EZntRRcW70i/RG/XrYnZvx5F9fBz9dOHZ6dPWLfLPl2Mzx6xTzRlb8iAVm120xgQmW+DyHwTRJKfnjR43lAmQLBtiVroi/09dL3Zc5cdyLWaHDqDw/Y7Hr1ta75yJjhOlFt39VozGrnYfXkIpS1sXVeNgxIDcF3m4bja8zUTxrpJ4xEiO2mr7Ll/f4o9Elb+tdeM5F+Z7LdD2lLo2+P5PoqNN+e7Ee4X/h/43groq3TvRfOd2KbrCVjw41yD6skDluvKYJ5bj+s2jnpxWHVUlNrgrYEW/pebFg2Xo5e/Mb2IP6o9eBTGNs4yNNsbYOSr7ivoGa5+Cg/HXu9bl5VfkrBXmObDgdDPuyUZoTN2z0fQ7eOjs/4dpw/G6sdJktzvRACnJx4m+amUb/142BaD9sE16LlsePQHTUVcJX4UAAA="
   def startLatitude = input["startLatitude"] as Double
@@ -211,7 +326,7 @@ public String getDump(DataSource dataSource, File zipFileName, Map input) {
             }
           }
         } catch (JsonException ex) {
-          logger.error(String.format("Track %d illegal content", track_row.getInt('pk_track')), ex);
+          LOGGER.error(String.format("Track %d illegal content", track_row.getInt('pk_track')), ex);
         }
       }
       fileJsonWriter << "]\n}\n"
@@ -331,7 +446,7 @@ public String getDump(DataSource dataSource, File zipFileName, Map input) {
   }
   // Move created files
   zipFileName.renameTo(new File(zipFileName.path.substring(0, zipFileName.path.length() - 4)))
-  logger.info(String.format("Dump complete \nTracks: %.2f seconds\nPoints %.2f seconds\nAreas %.2f seconds", totalDumpTracks / 1000, totalDumpPoints / 1000, totalDumpAreas / 1000))
+  LOGGER.info(String.format("Dump complete \nTracks: %.2f seconds\nPoints %.2f seconds\nAreas %.2f seconds", totalDumpTracks / 1000, totalDumpPoints / 1000, totalDumpAreas / 1000))
   return zipFileName.path.substring(0, zipFileName.path.length() - 4)
 }
 
@@ -339,14 +454,14 @@ def exec(Connection connection, Map input) {
   // Open PostgreSQL connection
   // Create dump folder
   def workingDir = System.getProperty("workingDir", "data_dir")
-  File dumpDir = Paths.get(workingDir,"onomap_area_dump").toFile()
+  File dumpDir = Paths.get(workingDir, "onomap_area_dump").toFile()
   if (!dumpDir.exists()) {
     dumpDir.mkdirs()
   }
 
   // create unique zip file
   def uuid = UUID.randomUUID().toString().replace("-", "")
-  File zipFileName = new File(dumpDir,  "extract_${uuid}.zip.tmp")
+  File zipFileName = new File(dumpDir, "extract_${uuid}.zip.tmp")
 
   Callable<String> task = new Callable<String>() {
     @Override
@@ -355,10 +470,10 @@ def exec(Connection connection, Map input) {
     }
   }
 
-  if("worker" in input) {
-      // Use special vert.x thread pool
-      def future = input["worker"].executeBlocking(task)
-      return [result: future]
+  if ("worker" in input) {
+    // Use special vert.x thread pool
+    def future = input["worker"].executeBlocking(task)
+    return [result: future]
   } else {
     def future = Executors.newSingleThreadExecutor().submit(task)
     return [result: future]
